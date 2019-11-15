c120313915b0a84b414265a4b1c1a8cc895d512f
Rodrigo Castro <rodrigocastro@claretiano.edu.br>
2019-11-15 20:29:47 -0300

    Seed file criando amostra de dados(db:setup)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in bibendum ipsum, non ornare sem. Integer non sem pretium justo volutpat vestibulum eu ut erat. Mauris tempor cursus diam id ultrices. Duis pretium, urna vel porta semper, diam sem mattis ex, quis tincidunt ex nulla nec odio. Suspendisse nulla tellus, facilisis id scelerisque vitae, porttitor sodales mi. Sed mollis pretium massa ac blandit. Maecenas auctor erat sed ex feugiat scelerisque. Proin dignissim justo bibendum, faucibus orci in, auctor felis. Donec in consequat quam."
+	)
+end
+puts "10 blogs created"
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}", 
+		percent_utilized: 15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio|
+	Portfolio.create!(
+		title:  "Portfolio title: #{portfolio}",
+    subtitle: "My Great Service",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in bibendum ipsum, non ornare sem. Integer non sem pretium justo volutpat vestibulum eu ut erat. Mauris tempor cursus diam id ultrices. Duis pretium, urna vel porta semper, diam sem mattis ex, quis tincidunt ex nulla nec odio. Suspendisse nulla tellus, facilisis id scelerisque vitae, porttitor sodales mi. Sed mollis pretium massa ac blandit. Maecenas auctor erat sed ex feugiat scelerisque. Proin dignissim justo bibendum, faucibus orci in, auctor felis. Donec in consequat quam.", 
+    main_image: "https://placehold.it/600x300",
+    thumb_image:"https://placehold.it/350x200"
+	)
+end
+puts "9 portfolios created"
