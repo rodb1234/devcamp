c37867594bf8d9fc76fb2cb51f96aaf02df75b3f
Rodrigo Castro <rodrigocastro@claretiano.edu.br>
2019-11-16 19:15:03 -0300

    Configurando acoes portfolio e routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,4 +18,37 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def update
+  	@portfolio = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio atualizado com sucesso!' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def edit
+  	@portfolio = Portfolio.find(params[:id])
+  end
+
+  def show
+  	@portfolio = Portfolio.find(params[:id])
+  end
+
+  def destroy
+  	#Procura o ID do portfolio para destruir
+  	@portfolio = Portfolio.find(params[:id])
+
+  	#Destroi/deleta o registro
+		@portfolio.destroy
+
+		#Redireciona para outra pagina
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Blog was removed.' }
+    end
+  end
 end
