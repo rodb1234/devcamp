68c3a8be1a0ef9342a0f634ea4bdc76338ac95f1
Rodrigo Castro <rodrigocastro@claretiano.edu.br>
2019-11-18 21:24:40 -0300

    Adicionando defaults para imagem do portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,14 +1,16 @@
 class Portfolio < ApplicationRecord
 
+	#validations
   validates :title, :body, :main_image, :thumb_image, presence: true
-  
-  def self.angular_def
-  	where(subtitle: 'angular')
-  end
 
+  #scopes
   scope :angular, -> { where(subtitle: 'angular') }
 
-  #add_scope :angular do
-  #  where(subtitle: 'angular')
-  #end
+  #callbacks
+  after_initialize :set_defaults
+
+  def set_defaults
+  	self.main_image ||= "https://placehold.it/600x300"
+  	self.thumb_image ||= "https://placehold.it/350x200"
+  end
 end
