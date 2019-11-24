fccf7eb67825c7e478c9437d44319a3e94834629
Rodrigo Castro <rodrigocastro@claretiano.edu.br>
2019-11-24 12:22:13 -0300

    usando render para e passando variavel entre classe css

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,23 +1,23 @@
 class Portfolio < ApplicationRecord
-	has_many :technologies, dependent: :destroy
+  has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
 
   before_destroy :no_referenced_orders
-	include Placeholder
+  include Placeholder
 
-	#validations
+  # validations
   validates :title, :body, :main_image, :thumb_image, presence: true
 
-  #scopes
+  # scopes
   scope :angular, -> { where(subtitle: 'angular') }
 
-  #callbacks
+  # callbacks
   after_initialize :set_defaults
 
-  #defaults
+  # defaults
   def set_defaults
-  	self.main_image ||= Placeholder.image_generator(600, 400)
-  	self.thumb_image ||= Placeholder.image_generator(350, 200)
+    self.main_image ||= Placeholder.image_generator(600, 400)
+    self.thumb_image ||= Placeholder.image_generator(350, 200)
   end
 end
