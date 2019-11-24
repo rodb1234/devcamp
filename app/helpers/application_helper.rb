3a513ba1b7f75c1caa0c3ea49844373d9153c4ca
Rodrigo Castro <rodrigocastro@claretiano.edu.br>
2019-11-24 17:05:56 -0300

    Implementando partial e partial com collection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
         (link_to 'Login', new_user_session_path, method: :get)
     end
   end
+
+  def sample_helper
+    content_tag(:div, 'My Content', class: 'my-class')
+  end
+
+  def source_helper
+    greeting = "Thanks for visiting me from #{session[:source]}"
+    content_tag(:p, greeting, class: 'source-greeting') if session[:source]
+  end
 end
