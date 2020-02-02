fa2ee6cea0f98f767b17273bd36aed59794b1fa7
Rodrigo Castro <rodrigocastro@claretiano.edu.br>
2020-02-02 12:03:50 -0300

    Adicionando estrutura dinamica para barra de navegacao

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,16 +19,38 @@ module ApplicationHelper
     content_tag(:p, greeting, class: 'source-greeting') if session[:source]
   end
 
-  def nav_helper style, tag_type
-nav_links = <<NAV
-<#{tag_type}><a href="#{root_path}" class="#{style}">Home</a></#{tag_type}>
-<#{tag_type}><a href="#{about_path}" class="#{style}">About me</a></#{tag_type}>
-<#{tag_type}><a href="#{contact_path}" class="#{style}">Contact</a></#{tag_type}>
-<#{tag_type}><a href="#{portfolios_path}" class="#{style}">Portfolios</a></#{tag_type}>
-NAV
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_path,
+        title: 'About '
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolios'
+      }
+    ]
+  end
 
+  def nav_helper(style, tag_type)
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
 
     nav_links.html_safe
   end
 
+  def active?(path)
+    'active' if current_page? path
+  end
 end
